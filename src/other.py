707206b2a7be8856c496d4a22d075670ea2433e8
Quynh Anh Do <doq300861@gmail.com>
2023-11-02 00:58:58 -0700

    add second page to mainwindow, delete unused ui

--- src/other.py
+++
@@ -1,6 +0,0 @@
-from PyQt5 import QtWidgets, uic
-
-class NewWindow(QtWidgets.QDialog):
-    def __init__(self):
-        super().__init__()
-        uic.loadUi('ui/other.ui', self)  # Load the .ui file for the new window
