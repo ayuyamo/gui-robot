c32bed60a08147edb325d29f07dbfcc9dcf101ad
Quynh Anh Do <doq300861@gmail.com>
2023-11-03 13:44:55 -0700

    add icons to main window

--- src/home_page/mainwindow.py
+++ src/home_page/mainwindow.py
@@ -2,6 +2,7 @@ from PyQt5 import QtWidgets, uic
 from home_page.navigate_pages import ButtonActions
 from home_page.add_pages import AddPage
 from home_page.launch_page import Launch
+from icons import Icons
 
 class MyWindow(QtWidgets.QMainWindow):
     def __init__(self):
@@ -9,12 +10,15 @@ class MyWindow(QtWidgets.QMainWindow):
         
         # Load the .ui file
         uic.loadUi('ui/mainwindow.ui', self)
-        
+
         add_page = AddPage(self)
         add_page.add_widgets()
 
         button_handler = ButtonActions(self)
         button_handler.connect_buttons()
+        
+        icons = Icons(self)
+        icons.setIcons()
         
         self.stacked_widget.setCurrentIndex(0)  # Set the initial page to index 0
         
